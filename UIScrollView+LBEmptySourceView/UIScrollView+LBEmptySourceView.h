8b3c1bb1c88d7b778197a5a7e8ec2ebb0dc59b02
刘彬 <1129434577@qq.com>
2020-06-02 16:29:33 +0800

    重传1.0.0

--- UIScrollView+LBEmptySourceView/UIScrollView+LBEmptySourceView.h
+++ UIScrollView+LBEmptySourceView/UIScrollView+LBEmptySourceView.h
@@ -13,9 +13,9 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIScrollView (LBEmptySourceView)
 @property (nonatomic, assign, readonly) NSUInteger dataSourceCount;//如果是UICollectionView和UITableView，即为数据源个数（包含所有header和footer），如果是UIScrollView，默认为1(默认对UIScrollView不控制)
 
-@property (nonatomic, assign) BOOL    emptySourceViewAvailable;//空视图开关，default YES
+@property (nonatomic, assign) BOOL    emptySourceViewAvailable;//空视图开关，default NO
 @property (nonatomic, assign) BOOL    allowShowEmptySourceViewWhenHaveHeaderOrFooter;//只针对UITableView有效，tableHeaderView和tableFooterView的时候空视图是否显示
-@property (nonatomic, strong) UIView *emptySourceView;//通过设置此属性自定义空时图样式
+@property (nonatomic, strong) UIView *emptySourceView;//通过设置此属性自定义空视图样式
 
 @end
 
